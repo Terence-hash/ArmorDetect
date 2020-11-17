494186bdfac637d03944d02d7d21065f3b606c96
Terence-hash <2228254095@qq.com>
2020-11-18 07:27:01 +0800

    header

---
+++ ArmorDetector.h
@@ -0,0 +1,26 @@
+﻿/*----------------------------
+ *   @file：     ArmorDetector.h
+ *   @date：     2020-7-10
+ *   @brief：    装甲板识别 头文件
+ *   @Author：   谭明瑞
+ *----------------------------
+ */
+#ifndef ARMORDETECTOR_H
+#define ARMORDETECTOR_H
+#include<opencv2/opencv.hpp>
+using namespace cv;
+
+
+class Detect{
+public:
+	RotatedRect adjustRec(RotatedRect& rec);
+	void process(Mat&src);
+	
+public:
+	int enemy_color;
+	int RED = 0;
+	int BLUE = 1;
+
+};
+#endif // !ARMORDETECTOR_H
+
